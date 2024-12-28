0cf0ec4f65784026e0497e619e784690f7002394
Subhajit Dhali <subhajitdhali0@gmail.com>
2024-12-28 13:28:49 +0400

    some css changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@ function loco() {
       return arguments.length
         ? locoScroll.scrollTo(value, 0, 0)
         : locoScroll.scroll.instance.scroll.y;
-    },
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
     getBoundingClientRect() {
       return {
         top: 0,
@@ -25,11 +25,11 @@ function loco() {
         height: window.innerHeight,
       };
     },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
     pinType: document.querySelector("#main").style.transform
       ? "transform"
       : "fixed",
   });
-  
 
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
